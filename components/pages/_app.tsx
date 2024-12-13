0d6f092580724917e284246bedf0e9e24a434f35
Arman Mondal <arman.mondal.27012005@gmail.com>
2024-12-13 12:42:34 +0530

    deleted:    components/pages/_app.tsx 	deleted:    components/pages/_document.tsx 	deleted:    components/pages/careers/_app.tsx 	deleted:    components/pages/careers/_document.tsx 	deleted:    components/pages/careers/index.tsx 	deleted:    components/pages/contacts/_app.tsx 	deleted:    components/pages/contacts/_document.tsx 	deleted:    components/pages/contacts/index.tsx 	deleted:    components/pages/index.tsx 	deleted:    components/pages/terms/_app.tsx 	deleted:    components/pages/terms/_document.tsx 	deleted:    components/pages/terms/index.tsx 	deleted:    components/pages/terms/terms.txt 	modified:   pages/_app.tsx 	modified:   pages/careers/_app.tsx 	modified:   pages/contacts/_app.tsx 	modified:   pages/terms/_app.tsx

--- components/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import type { AppProps } from 'next/app';
-import '../styles/tailwind.css'
-
-function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default App;
--- pages/terms/_app.tsx
+++ pages/terms/_app.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
+import  '../../styles/output.css';
 
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
